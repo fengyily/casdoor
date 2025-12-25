db3517828cd9bb6198f8d811c0aa2eb5354a9b8d
Fengyily <Fengyily@noreply.gitcode.com>
2025-12-25 23:46:09 +0800

    fix: correct field names for casdoor.Config struct

--- storage/casdoor.go
+++ storage/casdoor.go
@@ -7,13 +7,13 @@ import (
 
 func NewCasdoorStorageProvider(providerType string, clientId string, clientSecret string, region string, bucket string, endpoint string, cert string, content string) oss.StorageInterface {
 	sp := casdoor.New(&casdoor.Config{
-		ClientId:     clientId,
-		ClientSecret: clientSecret,
-		Endpoint:     endpoint,
-		Certificate:  cert,
-		Region:       region,
-		Content:      content,
-		Bucket:       bucket,
+		AccessID:         clientId,
+		AccessKey:        clientSecret,
+		Endpoint:         endpoint,
+		Certificate:      cert,
+		OrganizationName: region,
+		ApplicationName:  content,
+		Provider:         bucket,
 	})
 	return sp
 }
